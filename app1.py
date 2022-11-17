0945900e31a508e7b7d10f8aac83e56895c97754
Aruangshu Das <darunangshu2002@gmail.com>
2022-11-17 12:43:29 +0530

    first commit

---
+++ app1.py
@@ -0,0 +1,41 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180 / np.pi)
+    if (angle > 180):
+        angle = 360 - angle
+    return angle
+
+
+left_hand_side = 0
+right_hand_side = 0
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    _, frame = cap.read()
+    cv2.imshow('Detection model', frame)
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = pose.process()
+
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Pose detection
+
+    if cv2.waitKey(10) & 0XFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ readme.md
@@ -0,0 +1,7 @@
+This model will find out no of time you hand raise and down and your hand raise and down state
+
+**Step**
+
+- Run the code and raise hand down
+
+[**Project Demo Link**](https://youtu.be/g_flGAyo63E)
